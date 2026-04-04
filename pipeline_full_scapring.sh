95248b6ecae5153e5c3e93ac41e518c3552b222b
Gabriele <tomaigabriele@gmail.com>
2026-04-04 11:07:10 +0200

    update gitignore and pipeline with correct output folder

--- pipeline_full_scapring.sh
+++ pipeline_full_scapring.sh
@@ -4,7 +4,7 @@ set -e
 # --- Default parameters -- dates in ISO format ---
 START_DATE="2026-03-01"
 END_DATE="2026-03-08"
-OUTPUT_DIR="scraper_results_schedules_book_rooms_cources"
+OUTPUT_DIR="scraper_results_schedules_book_rooms_degrees_courses"
 
 ENV_DIR="env"
 REQUIREMENTS_FILE="requirements.txt"
@@ -37,41 +37,41 @@ else
     fi
 fi
 
-# --- Delete old scraper results ---
-printf "\nCleaning up old results...\n"
-rm -rf "$OUTPUT_DIR"
-mkdir -p "$OUTPUT_DIR"
+# # --- Delete old scraper results ---
+# printf "\nCleaning up old results...\n"
+# rm -rf "$OUTPUT_DIR"
+# mkdir -p "$OUTPUT_DIR"
 
-printf "\n\n\nADDRESS BOOK SCRAPER\n"
-python3 fetch_address_book.py --output="$OUTPUT_DIR/address_book.json"
-# python fetch_address_book.py --output="scraper_results_schedules_book_rooms_cources/address_book.json" --limit 1
+# printf "\n\n\nADDRESS BOOK SCRAPER\n"
+# python3 fetch_address_book.py --output="$OUTPUT_DIR/address_book.json"
+# # python fetch_address_book.py --output="scraper_results_schedules_book_rooms_degrees_courses/address_book.json" --limit 1
 
-printf "\n\n\nTeams codes scraper\n"
-python3 fetch_courses_with_teams_code.py -o "$OUTPUT_DIR/courses_with_teams_code.json"
-# python fetch_courses_with_teams_code.py -o "scraper_results_schedules_book_rooms_cources/courses_with_teams_code.json"
+# printf "\n\n\nTeams codes scraper\n"
+# python3 fetch_courses_with_teams_code.py -o "$OUTPUT_DIR/courses_with_teams_code.json"
+# # python fetch_courses_with_teams_code.py -o "scraper_results_schedules_book_rooms_degrees_courses/courses_with_teams_code.json"
 
-# printf "\n\n\bachelor, master and years masters' degree courses scraper\n"
-# python3 fetch_degree_programs_bachelor_master.py -o "$OUTPUT_DIR/full_courses.json"
-# # python3 fetch_degree_programs_bachelor_master.py -o "scraper_results_schedules_book_rooms_cources/degree_programs_old.json" --delay 0.15
+# # printf "\n\n\bachelor, master and years masters' degree courses scraper\n"
+# # python3 fetch_degree_programs_bachelor_master.py -o "$OUTPUT_DIR/full_courses.json"
+# # # python3 fetch_degree_programs_bachelor_master.py -o "scraper_results_schedules_book_rooms_degrees_courses/degree_programs_old.json" --delay 0.15
 
 printf "\n\n\nCourses IDs from catalogue\n"
 python3 fetch_course_ids_catalogue.py -o "$OUTPUT_DIR/course_list_ids.json"
-# python fetch_course_ids_catalogue.py -o "scraper_results_schedules_book_rooms_cources/course_list_ids.json"
+# python fetch_course_ids_catalogue.py -o "scraper_results_schedules_book_rooms_degrees_courses/course_list_ids.json"
 
 printf "\n\n\nDegrees and subjects from catalogue\n"
-python3 fetch_degrees_teachings.py.py -i "$OUTPUT_DIR/course_list_ids.json" --output-subjects "$OUTPUT_DIR/subjects.json" --output-degrees "$OUTPUT_DIR/degrees.json"
-# python fetch_degrees_teachings.py -i scraper_results_schedules_book_rooms_cources/course_list_ids.json --output-subjects scraper_results_schedules_book_rooms_cources/subjects.json --output-degrees scraper_results_schedules_book_rooms_cources/degrees.json --delay 0.15 --limit 5
+python3 fetch_degrees_teachings.py -i "$OUTPUT_DIR/course_list_ids.json" --output-subjects "$OUTPUT_DIR/subjects.json" --output-degrees "$OUTPUT_DIR/degrees.json"
+# python fetch_degrees_teachings.py -i scraper_results_schedules_book_rooms_degrees_courses/course_list_ids.json --output-subjects scraper_results_schedules_book_rooms_degrees_courses/subjects.json --output-degrees scraper_results_schedules_book_rooms_degrees_courses/degrees.json --delay 0.15 --limit 5
 
-printf "\n\n\nRooms info scraper\n"
-python3 fetch_info_rooms.py -o "$OUTPUT_DIR/info_rooms.json"
-# python fetch_info_rooms.py -o "scraper_results_schedules_book_rooms_cources/info_rooms.json" --limit 1
+# printf "\n\n\nRooms info scraper\n"
+# python3 fetch_info_rooms.py -o "$OUTPUT_DIR/info_rooms.json"
+# # python fetch_info_rooms.py -o "scraper_results_schedules_book_rooms_degrees_courses/info_rooms.json" --limit 1
 
-printf "\n\n\nROOMS CALENDAR\n"
-python3 fetch_rooms_calendar.py --start_date "$START_DATE" --end_date "$END_DATE" --output="$OUTPUT_DIR/rooms_calendar"
-# python fetch_rooms_calendar.py --start_date "2026-02-02" --end_date "2026-02-10" --output="scraper_results_schedules_book_rooms_cources/rooms_calendar" --num_sites 1
+# printf "\n\n\nROOMS CALENDAR\n"
+# python3 fetch_rooms_calendar.py --start_date "$START_DATE" --end_date "$END_DATE" --output="$OUTPUT_DIR/rooms_calendar"
+# # python fetch_rooms_calendar.py --start_date "2026-02-02" --end_date "2026-02-10" --output="scraper_results_schedules_book_rooms_degrees_courses/rooms_calendar" --num_sites 1
 
-printf "\n\n\nLESSONS CALENDAR SCARPER\n"
-python3 fetch_lessons_calendar.py --start_date "$START_DATE" --end_date "$END_DATE" --output="$OUTPUT_DIR/lessons_calendar"
-# python fetch_lessons_calendar.py --start_date "2026-03-02" --end_date "2026-03-10" --output="scraper_results_schedules_book_rooms_cources/lessons_calendar" --num_departments 1
+# printf "\n\n\nLESSONS CALENDAR SCARPER\n"
+# python3 fetch_lessons_calendar.py --start_date "$START_DATE" --end_date "$END_DATE" --output="$OUTPUT_DIR/lessons_calendar"
+# # python fetch_lessons_calendar.py --start_date "2026-03-02" --end_date "2026-03-10" --output="scraper_results_schedules_book_rooms_degrees_courses/lessons_calendar" --num_departments 1
 
 printf "\n\n\nSCRAPING ENDED\n"
